98f4b988f6fd1f951bf37d8623ec90d8264d8e35
Mrowr Purr <mrowr.purr@gmail.com>
2025-05-09 19:05:49 -0700

    tool

--- my_first_adk/agent.py
+++ my_first_adk/agent.py
@@ -1,13 +1,23 @@
 from dotenv import find_dotenv, load_dotenv
-from google.adk import Agent  # type: ignore
-from google.adk.models.lite_llm import LiteLlm  # type: ignore
+from google.adk import Agent
+from google.adk.models.lite_llm import LiteLlm
 
 load_dotenv(find_dotenv())
+
+
+def get_users_name() -> str:
+    """
+    Get the user's name.
+    """
+    return "The user's name is Sally Smith the First."
+
 
 root_agent = Agent(
     name="anything_agent",
     model=LiteLlm("openrouter/openai/o3-mini"),
     description="Agent to answer questions.",
-    instruction="I can answer your questions.",
-    tools=[],
+    instruction="""
+    I can answer your questions.
+    """,
+    tools=[get_users_name],
 )
